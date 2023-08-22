73be737f487d5bb456d03fd79ea8af2e27d10250
Ross Buchan <rossbuchan1@googlemail.com>
2023-08-22 16:36:28 +0100

    Fix end of quiz bug and display score

--- src/containers/QuizContainer.jsx
+++ src/containers/QuizContainer.jsx
@@ -24,29 +24,49 @@ const QuizContainer = () => {
     )
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-
+    const [currentQuestionNumber, setCurrentQuestionNumber] = useState(1)
     const [selectedAnswer, setSelectedAnswer] = useState("")
+    const [score, setScore] = useState(0)
+    // const [answers, setAnswers] = useState(questions[currentQuestionIndex])
 
-    const answers = questions[currentQuestionIndex].options.map((option, index) => 
-        <div key={index}>
-            <label htmlFor={index}>{option}</label>
-            <input type="radio" id={index} name="options" onChange={(event) => setSelectedAnswer(option)}></input>
-        </div>
+    const endOfQuiz = () => {
+        return (
+            <>
+                <h2>Congratulations you've finished the quiz</h2>
+                <h2>Your score is {score}</h2>
+            </>
+        )
+    }
+    
+    const answers = (currentQuestionIndex == (questions.length)) 
+        ? endOfQuiz() 
+        : questions[currentQuestionIndex].options.map((option, index) => 
+            <div key={index}>
+                <label htmlFor={index}>{option}</label>
+                <input type="radio" id={index} name="options" onChange={(event) => setSelectedAnswer(option)}></input>
+            </div>
     )
 
-    const handleAnswer = () => {
+    const handleAnswer = (event) => {
         event.preventDefault()
+        // currentQuestionIndex == (questions.length - 1) ? endOfQuiz() : 
         setCurrentQuestionIndex(currentQuestionIndex + 1)
+        setCurrentQuestionNumber(currentQuestionNumber + 1)
+        setScore((selectedAnswer == questions[currentQuestionIndex].answer ? score + 1 : score))
     }
+
+    console.log(currentQuestionIndex)
+    console.log(questions.length)
 
 
 
     return (
         <>
-            <h3>{questions[currentQuestionIndex].question}</h3>
+            <h3>Score: {score}</h3>
+            <h3>Current Question: {currentQuestionNumber}</h3>
             <form>
                 {answers}
-                <button type="submit" onClick={handleAnswer}/>
+                <button type="submit" onClick={handleAnswer}>Next Question</button>
             </form>
         </>
     )
